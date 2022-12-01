b6d611519ccfe5ddb6f42c6e27d1334c27b43dce
rubayet-islam <season.ss2001@gmail.com>
2022-12-01 15:56:11 +0600

    navbar done

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
-    return (
-        <div>
-            this is nav
-        </div>
-    );
+  return (
+    <div className="flex justify-between items-center w-full z-[100] px-5 pt-5">
+      <div>
+        <h1 className="text-xl md:text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-br cursor-pointer from-red-600 to-red-900">
+          NETFLIX
+        </h1>
+      </div>
+      <div>
+        <button className="text-white mr-4">Sign In</button>
+        <button className="bg-red-600 py-2 px-3 text-white">Sign Up</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
