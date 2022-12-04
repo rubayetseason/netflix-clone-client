8b631b15a3bf387d100a899435852466018cff70
rubayet-islam <season.ss2001@gmail.com>
2022-12-04 22:09:21 +0600

    random image selection done with responsive image display

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -17,7 +17,21 @@ const Gallery = () => {
     galleryMovies[Math.floor(Math.random() * galleryMovies.length)];
   console.log(galleryMovie);
 
-  return <div>this is gallery</div>;
+  return (
+    <div>
+      <section className="gallery-section">
+        <div className="w-full h-[500px]">
+          <div className="w-full h-full">
+            <img
+              src={`https://image.tmdb.org/t/p/original/${galleryMovie?.backdrop_path}`}
+              className="object-cover h-[450px] md:h-full w-full"
+              alt={galleryMovie?.title}
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Gallery;
