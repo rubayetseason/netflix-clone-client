57dec821e9bd10ba44aaefa9d9bd341c35155319
rubayet-islam <season.ss2001@gmail.com>
2022-12-04 19:08:57 +0600

    tanstack query installed and implemented in the index.js file

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import AuthProvider from "./context/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <AuthProvider>
-    <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
       <App />
-    </React.StrictMode>
-  </AuthProvider>
+    </AuthProvider>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
