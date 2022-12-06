4b260ec3eaaec28564e8ce6e552d762849e5bfed
rubayet-islam <season.ss2001@gmail.com>
2022-12-06 19:27:36 +0600

    installed react youtube and react trailer package

--- src/components/shared/Row.js
+++ src/components/shared/Row.js
@@ -3,12 +3,16 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import Movie from "./Movie";
 import "./Row.css";
+import movieTrailer from "movie-trailer";
 
 const Row = ({ genre, fetchUrl }) => {
   const [rowMovies, setRowMovies] = useState([]);
 
   const movieHandler = (movie) => {
-    console.log(movie);
+    movieTrailer(movie.title).then((res) => {
+      const ytUrl = res;
+      const trailerUrl = ytUrl.substr(32);
+    });
   };
 
   useEffect(() => {
